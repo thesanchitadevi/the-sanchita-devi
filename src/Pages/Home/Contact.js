731ddee37631be34ab69644a2402b48923aa3a70
sanchitadevi <thesanchitadevi@gmail.com>
2022-12-10 18:19:33 +0600

    contact page

---
+++ src/Pages/Home/Contact.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <section className="py-6  text-cyan-700">
+            <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
+                <div className="py-6 md:py-0 md:px-6">
+                    <h1 className="text-4xl font-bold">Get in touch</h1>
+                    <p className="pt-2 pb-4">Fill in the form to start a conversation</p>
+                    <div className="space-y-4">
+                        <p className="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                                <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"></path>
+                            </svg>
+                            <span>Dhaka, Bangladesh</span>
+                        </p>
+                        <p className="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
+                            </svg>
+                            <span>(+88)01300306956</span>
+                        </p>
+                        <p className="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                            </svg>
+                            <span>thesanchitadevi@gmail.com</span>
+                        </p>
+                    </div>
+                </div>
+                <form novalidate="" className="flex flex-col py-6 space-y-6 md:py-0 md:px-6 ng-untouched ng-pristine ng-valid">
+                    <label className="block">
+                        <span className="mb-1">Full name</span>
+                        <input placeholder='your name' className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 mt-5" type="text" />
+                    </label>
+                    <label className="block">
+                        <span className="mb-1">Email address</span>
+                        <input placeholder='name@mail.com' className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 mt-5" type="email"/>
+                    </label>
+                    <label className="block">
+                        <span className="mb-1">Message</span>
+                        <textarea rows="3" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 mt-5"></textarea>
+                    </label>
+                    <button type="button" className="self-center px-8 py-2 text-lg rounded  bg-cyan-700 text-cyan-100 ">Submit</button>
+                </form>
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
