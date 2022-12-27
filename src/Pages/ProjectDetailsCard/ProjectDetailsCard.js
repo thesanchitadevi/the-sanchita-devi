a2e9a2a0068fd1cc947db987c3129c4bf470fa77
sanchitadevi <thesanchitadevi@gmail.com>
2022-12-28 01:40:46 +0600

    card details changed

--- src/Pages/ProjectDetailsCard/ProjectDetailsCard.js
+++ src/Pages/ProjectDetailsCard/ProjectDetailsCard.js
@@ -20,32 +20,50 @@ const ProjectDetailsCard = ({ site }) => {
             </header>
 
             <>
-                <div className="container mx-auto space-y-12 mt-10">
-                    <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
-                        <img src={img1} alt="" className="h-80 bg-gray-500 aspect-video "/>
-                        <div className="flex flex-col justify-center flex-1 p-6 ">
-                            <span className="text-xs uppercase text-gray-400">Join, it's free</span>
-                            <h3 className="text-3xl font-bold">We're not reinventing the wheel</h3>
-                            <p className="my-6 text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
+                <section
+                    class="overflow-hidden  sm:grid sm:grid-cols-2 sm:items-center"
+                >
+                    <div class="p-8 md:p-12 lg:px-16 lg:py-24">
+                        <div class="mx-auto max-w-xl text-center sm:text-left">
+                            <h2 class="text-xl font-bold text-gray-100 md:text-3xl">
+                                PROJECT INFO
+                            </h2>
+
+                            <p class="hidden text-gray-500 md:mt-4 md:block">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas
+                                tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim et
+                                fermentum, augue. Aliquet amet volutpat quisque ut interdum tincidunt
+                                duis.
+                            </p>
+                            <h2 class="text-xl font-bold text-gray-100 md:text-3xl mt-10">
+                                SKILL USED
+                            </h2>
+
+                            <p class="hidden text-gray-500 md:mt-4 md:block">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas
+                                tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim et
+                                fermentum, augue. Aliquet amet volutpat quisque ut interdum tincidunt
+                                duis.
+                            </p>
+
+                            <div class="mt-4 md:mt-8">
+                                <a
+                                    href="#"
+                                    class="inline-block rounded bg-emerald-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-yellow-400"
+                                >
+                                    Get Started Today
+                                </a>
+                            </div>
                         </div>
                     </div>
-                    <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row-reverse">
-                        <img src={img2} alt="" className="h-80 bg-gray-500 aspect-video" />
-                        <div className="flex flex-col justify-center flex-1 p-6 ">
-                            <span className="text-xs uppercase text-gray-400">Join, it's free</span>
-                            <h3 className="text-3xl font-bold">We're not reinventing the wheel</h3>
-                            <p className="my-6 text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
-                        <img src={img3} alt="" className="h-80 bg-gray-500 aspect-video" />
-                        <div className="flex flex-col justify-center flex-1 p-6 ">
-                            <span className="text-xs uppercase text-gray-400">Join, it's free</span>
-                            <h3 className="text-3xl font-bold">We're not reinventing the wheel</h3>
-                            <p className="my-6 text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
-                        </div>
-                    </div>
-                </div>
+
+                    <img
+                        alt="Violin"
+                        src={img1}
+                        class="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-tl-[30px] md:h-[calc(100%_-_4rem)] md:rounded-tl-[60px]"
+                    />
+                </section>
+
             </>
         </div>
     );
